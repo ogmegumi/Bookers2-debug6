56eae96b7995bc5d666a1ffa63339f8b1eca3289
ogmegumi <ogmegumi@gmail.com>
2021-07-06 10:39:20 +0000

    [finish]ouyou-6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,25 +1,25 @@
 class BookCommentsController < ApplicationController
 	before_action :authenticate_user!
-
+	
   def create
     @book = Book.find(params[:book_id])
     @book_comment = BookComment.new(book_comment_params)
     @book_comment.book_id = @book.id
     @book_comment.user_id = current_user.id
-    @book_comments = @book.book_comments
     @book_comment.save
     #redirect_to book_path(@book.id)
   end
-
+  
   def destroy
+   @book = Book.find(params[:book_id])
    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
    #redirect_to book_path(params[:book_id])
   end
-
+  
   private
 
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-
+  
 end
