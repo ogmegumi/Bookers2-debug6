40cdb537bd6f695499128dfce2f6bc83b99d8107
ogmegumi <ogmegumi@gmail.com>
2021-07-06 07:18:21 +0000

    [update]ouyou-6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,17 @@
 class FavoritesController < ApplicationController
-   def create
-     @book = Book.find(params[:book_id])
-     @favorite = current_user.favorites.new(book_id: @book.id)
-     @favorite.save
-     redirect_back(fallback_location: root_path)
-   end
+  before_action :authenticate_user!
 
-   def destroy
-     @book = Book.find(params[:book_id])
-     @favorite = current_user.favorites.find_by(book_id: @book.id)
-     @favorite.destroy
-     redirect_back(fallback_location: root_path)
-     #自分のコメントしか削除できないこと
-     #コメント, コメント削除後は行う前の画面に遷移
-   end
+  def create
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.new(user_id: current_user.id)
+    favorite.save
+    # redirect_to request.referer
+  end
+
+  def destroy
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.find_by(user_id: current_user.id)
+    favorite.destroy
+    # redirect_to request.referer
+  end
 end
